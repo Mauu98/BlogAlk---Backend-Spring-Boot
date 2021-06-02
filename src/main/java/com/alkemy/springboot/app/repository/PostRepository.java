e8b67906359d565be30e9aaf8aa12caf2e7d372d
Mauri Pino <61031521+Mauu98@users.noreply.github.com>
2021-06-02 12:32:42 -0300

    Update PostRepository.java
    Se modificó @Query en PostRepository.

--- src/main/java/com/alkemy/springboot/app/repository/PostRepository.java
+++ src/main/java/com/alkemy/springboot/app/repository/PostRepository.java
@@ -13,10 +13,10 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     @Query("SELECT p FROM post p WHERE p.title = ?1")
     public List<Post> searchByTitle(String title);
 
-    @Query("SELECT p, c.category_name FROM post p INNER JOIN category c ON p.category_id WHERE c.category_name = ?1")
+    @Query("SELECT p.title, c.category_name FROM post p INNER JOIN category c ON p.category_id WHERE c.category_name = ?1")
     public List<Post> searchByCategory(String category);
 
-    @Query("SELECT p, c.category_name FROM post p INNER JOIN category c ON p.category_id WHERE p.title = ?1 AND c.category_name = ?1")
+    @Query("SELECT p.title, c.category_name FROM post p INNER JOIN category c ON p.category_id WHERE p.title = ?1 AND c.category_name = ?1")
     public List<Post> searchByTitleAndCategory(String title, String category);
 
     @Query(value = "INSERT INTO user_posts (user_id, posts_id) VALUES (:user_id, :posts_id)", nativeQuery = true)
